787e6116e3c5e341ccf8e6a436625f4d9dd4726e
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-09-30 21:54:18 +0530

    ~

---
+++ Advanced/import.py
@@ -0,0 +1,11 @@
+import export
+
+name = input("Enter your name: ")
+greeting = export.greet(name)
+print(greeting)
+
+num = int(input("Enter a number: "))
+result = export.num(num)
+print("Square:", result)
+
+export.number = export.Number(555)
