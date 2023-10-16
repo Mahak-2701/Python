3aff0bc6441af78cdf4f2e42ec7d07942b7a0b21
Mahak <sainimahak004@gmail.com>
2023-10-16 11:29:30 +0530

    Update evenodd_list.py

--- programs/evenodd_list.py
+++ programs/evenodd_list.py
@@ -2,5 +2,9 @@ def check(num):
     for i in num:
         if(i%2==0):
             print(i)
-        else:
+    for i in num:
+        if(i%2!=0):
             print(i)
+
+number=[2,5,8,6]
+check(number)
