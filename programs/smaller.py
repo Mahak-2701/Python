66717d9f74aadd834a1a2e68377c9bb8073ef169
Mahak <sainimahak004@gmail.com>
2023-10-20 16:58:11 -1000

    Update smaller.py

--- programs/smaller.py
+++ programs/smaller.py
@@ -1,6 +1,9 @@
-my_list=[2,5,6,1]
-smallest=my_list[0]
-for i in my_list:
-    if i<smallest:
-        smallest=i
-print("smallest no is",smallest)
+# my_list=[2,5,6,1]
+# smallest=my_list[0]
+# for i in my_list:
+#     if i<smallest:
+#         smallest=i
+# print("smallest no is",smallest)
+
+my_list=[2,5,1,7]
+print("smallest number in list is:",min(my_list))
