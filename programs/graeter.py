03c3a18358373f85179108ed38096d3bbe660ad6
Mahak <sainimahak004@gmail.com>
2023-10-20 16:59:03 -1000

    Create graeter.py

---
+++ programs/graeter.py
@@ -0,0 +1,9 @@
+# my_list=[2,5,6,1]
+# smallest=my_list[0]
+# for i in my_list:
+#     if i<smallest:
+#         smallest=i
+# print("smallest no is",smallest)
+
+my_list=[2,5,1,7]
+print("smallest number in list is:",max(my_list))
