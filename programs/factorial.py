9f5a15ef1924b26bade88c7817c43f3df71ede9d
Mahak <sainimahak004@gmail.com>
2023-10-25 12:46:15 +0530

    Update factorial.py

--- programs/factorial.py
+++ programs/factorial.py
@@ -1,11 +1,19 @@
-num = int(input("Enter a number: ")) 
-factorial = 1 
-    if num < 0: 
- print(" Factorial does not exist for negative numbers") 
-    elif num == 0: 
- print("The factorial of 0 is 1") 
-    else: 
-     for i in range(1,num + 1): 
-        factorial = factorial*i 
-        print("The factorial of",num,"is",factorial)
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers"
+    elif n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+
+# Input a number
+num = int(input("Enter a number: "))
+
+# Calculate and print the factorial
+fact = factorial(num)
+print(f"The factorial of {num} is {fact}")
+
 
