27c17427a64a82e5ebcb181b05325d33e217c6d0
Mahak <sainimahak004@gmail.com>
2023-10-24 18:34:21 -1200

    Update factorial.js

--- programs/factorial.js
+++ programs/factorial.js
@@ -1,11 +1,2 @@
-num=int(input("Eneter a number:"))
-factorial=1
-if num<0:
-    print("factorial doesn't exists for negative number:")
-elif num==0:
-    print("The factorial of 0 is 1")
-else:
-    for i in Range(1,num+1):
-        factorial*=i
-    print("the factoral of ",num ,"is",factorial)
+
 
