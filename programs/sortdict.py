cae35d7a2a3f4eaa066aabc2e6f11a4f4a3c5f62
Mahak <sainimahak004@gmail.com>
2023-11-06 15:03:59 +0530

    Create sortdict.py

---
+++ programs/sortdict.py
@@ -0,0 +1,7 @@
+# 20. Write a program to sort dictionary by key 
+myDict = {'ravi': 10, 'rajnish': 9, 
+ 'sanjeev': 15, 'yash': 2, 'suraj': 32} 
+myKeys = list(myDict.keys()) 
+myKeys.sort() 
+sorted_dict = {i: myDict[i] for i in myKeys} 
+print(sorted_dict) 
