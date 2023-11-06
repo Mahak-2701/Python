9989807a173a119cde57c8b61d3b8b28a19943a0
Mahak <sainimahak004@gmail.com>
2023-11-06 15:14:42 +0530

    Update mergedict.py

--- programs/mergedict.py
+++ programs/mergedict.py
@@ -3,6 +3,6 @@ def Merge(dict1, dict2):
  return(dict2.update(dict1)) 
 # Driver code 
 dict1 = {'a': 10, 'b': 8} 
-# dict2 = {'d': 6, 'c': 4} 
-# print(Merge(dict1, dict2)) 
+dict2 = {'d': 6, 'c': 4} 
+print(Merge(dict1, dict2)) 
 print(dict2)
