dc798f00436b5db94b2f4a7274f07db33e47f8b7
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-12-21 17:45:14 -1200

    py

--- programs/graeter.py
+++
@@ -1,9 +0,0 @@
-my_list=[2,5,6,1]
-graeter=my_list[0]
-for i in my_list:
-    if i>graeter:
-        greater=i
-print("greater no is",greater)
-
-# my_list=[2,5,1,7]
-# print("smallest number in list is:",max(my_list))
